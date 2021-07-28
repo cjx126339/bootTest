d8da27003a5464d1e35c90b9ec02ffed1a9de720
jiaxing <1263396150@qq.com>
2021-07-29 00:43:39 +0800

    master 第二次添加 哈哈哈

--- src/main/java/com/jfcat/boottest/BootTestApplication.java
+++ src/main/java/com/jfcat/boottest/BootTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootTestApplication {
 
     public static void main(String[] args) {
+        System.out.println("master 第一次添加");
         SpringApplication.run(BootTestApplication.class, args);
     }
 
